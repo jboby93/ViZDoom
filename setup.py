0ea8103f80c858f1167a36930cbeb30e752f91a1
mwydmuch <marek@wydmuch.poznan.pl>
2020-02-27 13:18:46 +0100

    New .travis.yml config, update setup.py

--- setup.py
+++ setup.py
@@ -111,10 +111,10 @@ setup(
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     keywords=['vizdoom', 'doom', 'ai', 'deep learning', 'reinforcement learning', 'research']
 )
